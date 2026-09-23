212a847445185f50cbc47029c928c8d42d63ff66
mac <@b.tunde.ipaye@gmail.com>
2026-09-23 18:56:34 +0100

    feat: App controller logic impl commit by ENGR. IPAYE

---
+++ backend/src/main/java/dev/engripaye/backend/identity/AuthController.java
@@ -0,0 +1,15 @@
+package dev.engripaye.backend.identity;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+}
