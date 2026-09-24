b4e5c6ba12bbaefa6b5a20be95a6f4ac5f3cae04
mac <@b.tunde.ipaye@gmail.com>
2026-09-24 12:52:52 +0100

    feat: jwt service, secret key impl. commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/identity/JwtService.java
+++ backend/src/main/java/dev/engripaye/backend/identity/JwtService.java
@@ -1,9 +1,23 @@
 package dev.engripaye.backend.identity;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
+
+    private final SecretKey secretKey;
+
+    private final long minutes;
+
+    JwtService(@Value("${app.jwt.secret}")
+               String secret,
+               @Value("${app.jwt.access-token-minutes}")
+               long minutes) {
+
+    }
 
 
 }
