23e83372eceb6cf50cc88c769dc056b45bf89481
mac <@b.tunde.ipaye@gmail.com>
2026-09-24 12:40:09 +0100

    feat: jwt auh, security context logic updated commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/identity/JwtAuthenticationFilter.java
+++ backend/src/main/java/dev/engripaye/backend/identity/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -31,7 +32,7 @@ class JwtAuthenticationFilter extends OncePerRequestFilter {
                         UUID id = jwt.parseSubject(value.substring(7));
 
                         var auth = new UsernamePasswordAuthenticationToken(id, null, java.util.List.of());
-                        auth.setDetails();
+                        auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     }
                 }
     }
