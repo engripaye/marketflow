2d28b724adb3f1b98665448854221d90d48bb437
mac <@b.tunde.ipaye@gmail.com>
2026-09-25 23:30:06 +0100

    feat: api exceptional handler, problem detail logic updated commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/common/ApiExceptionalHandler.java
+++ backend/src/main/java/dev/engripaye/backend/common/ApiExceptionalHandler.java
@@ -1,7 +1,18 @@
 package dev.engripaye.backend.common;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 class ApiExceptionalHandler {
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    ProblemDetail notFound(ChangeSetPersister.NotFoundException e) {
+        return problem(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+
 }
