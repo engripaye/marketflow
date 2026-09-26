87090568dbc2604e8e4ff79e1a865962bb97a7fd
mac <@b.tunde.ipaye@gmail.com>
2026-09-26 16:06:45 +0100

    feat: catalogue product, unit price impl. commit by ENGR. IPAYE

--- backend/src/main/java/dev/engripaye/backend/catalog/Product.java
+++ backend/src/main/java/dev/engripaye/backend/catalog/Product.java
@@ -7,6 +7,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Getter
 @NoArgsConstructor( access = AccessLevel.PROTECTED)
 @Entity
@@ -25,19 +27,24 @@ public class Product extends BaseEntity {
 
     private String description;
 
+    @Column(name="unit_price",nullable=false,precision=19,scale=2)
+    private BigDecimal unitPrice;
+
     @Column(name="reorder_level",nullable=false)
     private int reorderLevel;
 
     @Column(nullable=false)
     private boolean active=true;
 
-    public Product(String name, String sku, String description, int reorderLevel) {
+    public Product(String name, String sku, String description, BigDecimal unitPrice, int reorderLevel) {
         this.name = name;
         this.sku = sku;
         this.description = description;
+        this.unitPrice = unitPrice;
         this.reorderLevel = reorderLevel;
     }
 
+    public void update(String name, String sku, String description, int reorderLevel) {}
 
 
 }
